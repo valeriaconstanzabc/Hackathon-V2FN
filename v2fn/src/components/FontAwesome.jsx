8e37f3cd7b12ffb541f02a904fc76904b45236b6
Francisca <franciscabelen112002@gmail.com>
2020-09-25 17:51:09 -0300

    confirmar

---
+++ v2fn/src/components/FontAwesome.jsx
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUtensils, faHamburger, faTrash, faArrowCircleLeft, faArrowCircleRight} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUtensils, faHamburger, faTrash, faArrowCircleLeft, faArrowCircleRight)
