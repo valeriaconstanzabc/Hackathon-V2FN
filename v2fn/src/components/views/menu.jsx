a8503d5423386577ca993f633a12267029483033
Francisca <franciscabelen112002@gmail.com>
2020-09-28 11:37:27 -0300

    confirmando

--- v2fn/src/components/views/menu.jsx
+++ v2fn/src/components/views/menu.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Link } from "react-router-dom";
-import { Navbar } from 'react-bootstrap/Navbar';
 
 const Menu = (props) => {
 
 return (
-  <nav className="navbar">
 
-<div class="navmenu navmenu-default navmenu-fixed-left offcanvas-sm">
-  <a class="navmenu-brand visible-md visible-lg" href="#"> <img src="https://i.ibb.co/0ZHm0t0/EY-Logo-Beam-Tag-Stacked-RGB-EN-1.png"></img> </a>
-  <ul class="nav navmenu-nav">
-    <li><a href="#">Link</a></li>
-    <li><a href="#">Link</a></li>
-    <li><a href="#">Link</a></li>
-  </ul>
+<div class="row">
+  <div class="col-3">
+    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    <a class="nav-link" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true"> 
+      <img src="https://i.ibb.co/0ZHm0t0/EY-Logo-Beam-Tag-Stacked-RGB-EN-1.png">
+      </img> 
+      </a>
+      <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">
+        Home
+      </a>
+      <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">
+        Profile
+      </a>
+      <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">
+        Messages
+      </a>
+      <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">
+        Settings
+      </a>
+    </div>
+  </div>
 </div>
-
-<div class="navbar navbar-default navbar-fixed-top hidden-md hidden-lg">
-  <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target=".navmenu">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-  <a class="navbar-brand" href="#">Project name</a>
-</div>
-
-</nav>
 
 )   
 }
