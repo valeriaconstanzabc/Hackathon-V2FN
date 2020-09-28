02a6440e85d3cd06a4d205ae9adf9ed0938beac7
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-09-27 21:27:21 -0300

    cambios estilo barra nav

--- v2fn/src/components/Nav.jsx
+++ v2fn/src/components/Nav.jsx
@@ -22,20 +22,31 @@ const Nav = (props) => {
                     <img alt="lupa" src="https://i.ibb.co/FHJZfFn/Vector.png" className="image_buscar"/>
                     <input type="text" className="inputNav"/>
                 </div>
-                <div className="containerBtnsNav">
-                    <button className="btnNav"><img className="imgNav" src="https://i.ibb.co/JtqnbfN/Group-1.png" alt=""/></button>
-                    <button className="btnNav"><img className="imgNav" src="https://i.ibb.co/ts5QJbd/Vector-2.png" alt=""/></button>
-                </div>
-                <div className="containerImgUserNav">
-                    {
-                        user.photoURL === null ?
-                        <img type ="button" className="imgUserNav" alt="img" src="https://i.ibb.co/RDg954s/usuario-sin-foto.png"/>
-                        :
-                        <img type ="button" className="imgUserNav" alt="img" src={user.photoURL}/>  
-                    }
-                    <button className="btnNav"><img className="imgArrowNav" src="https://i.ibb.co/dL5nwZg/Vector-1.png" alt=""/></button>
-                    <div className="dropdownContent">
-                        <button onClick={() => cerrarSesion()} className="logout">Cerrar sesión</button>  
+                <div className="containerNavTwo">
+                    <div className="containerBtnsNav">
+                        <button className="btnNav"><img className="imgNav" src="https://i.ibb.co/JtqnbfN/Group-1.png" alt=""/></button>
+                        <button className="btnNav"><img className="imgNav" src="https://i.ibb.co/7KGG9Ry/Group.png" alt=""/></button>
+                            <p className="btnNav">#1</p>
+                    </div>
+                    <div className="containerImgUserNav">
+                        <div className="containerImgUserNav2">
+                            {
+                                user.photoURL === null ?
+                                <img type ="button" className="imgUserNav" alt="img" src="https://i.ibb.co/RDg954s/usuario-sin-foto.png"/>
+                                :
+                                <img type ="button" className="imgUserNav" alt="img" src={user.photoURL}/>  
+                            }
+                            <div className="containerNameAndPoints">
+                                <h5 className="nameNav">{user.displayName}</h5>
+                                <p className="pointsNav">Nivel 1 - {user.suma} pts.</p>
+                            </div>
+                        </div>
+                        <div className="containerSignOutUserNav">
+                            <button className="btnNav"><img className="imgNav" src="https://i.ibb.co/ts5QJbd/Vector-2.png" alt=""/></button>
+                            <div className="dropdownContent">
+                                <button onClick={() => cerrarSesion()} className="logout">Cerrar sesión</button>  
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
