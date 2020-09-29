7e47b219d47bbf7cfeed51f1295a27448ac40b43
estefania moyano <estefaniamoyano@Airdeestefania.home>
2020-09-29 17:21:13 -0300

    tablero ok

--- v2fn/src/components/list/List.jsx
+++ v2fn/src/components/list/List.jsx
@@ -4,30 +4,49 @@ import { makeStyles } from '@material-ui/core/styles';
 import Title from './Title';
 import Card from './Card';
 import InputContainer from '../input/InputContainer';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
 
 // Estilos de TODO//
-const useStyle = makeStyles((theme)=>({
-    root:{
+const useStyle = makeStyles((theme) => ({
+    root: {
         width: '300px',
         backgroundColor: '#EBECF0',
         marginLeft: theme.spacing(1),
     },
+    cardContainer:{
+        marginTop:theme.spacing(4),
+
+    }
 
 }));
-export default function List({ list }) {
+export default function List({ list, index }) {
     const classes = useStyle();
     console.log(list.title)
     return (
-        <div>
-            <Paper className= {classes.root}>
+        <Draggable draggableId={list.id} index={index}>
+            {(provided)=> (
+        <div {...provided.draggableProps} ref={provided.innerRef}>
+            <Paper className={classes.root} {...provided.dragHandleProps}>
                 <CssBaseline />
-                <Title title={list.title} />
-                {list.cards.map((card)=>(
-                    <Card key={card.id} card={card}/>
-                ))}
-                <InputContainer listId={list.id} type="card"/>
+                <Title title={list.title} listId={list.id}/>
+                <Droppable droppableId={list.id}>
+                    {(provided) => (
+                        <div
+                        ref={provided.innerRef} 
+                        {...provided.droppableProps} 
+                        className={classes.cardContainer}
+                        >
+                            {list.cards.map((card, index) => (
+                                <Card key={card.id} card={card} index={index} />
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+                <InputContainer listId={list.id} type="card" />
             </Paper>
-            
         </div>
+        )}
+        </Draggable>
     );
 }
--- v2fn/src/utils/store.js
+++ v2fn/src/utils/store.js
@@ -17,16 +17,31 @@ const data = {
   lists: {
     'list-1': {
       id: 'list-1',
-      title: 'Todo',
+      title: 'Backlog',
       cards,
     },
     'list-2': {
       id: 'list-2',
+      title: 'Todo',
+      cards: [],
+    },
+    'list-3': {
+      id: 'list-3',
       title: 'Doing',
       cards: [],
     },
+    'list-4': {
+      id: 'list-4',
+      title: 'Review',
+      cards: [],
+    },
+    'list-5': {
+      id: 'list-5',
+      title: 'Done',
+      cards: [],
+    },
   },
-  listIds: ['list-1', 'list-2'],
+  listIds: ['list-1', 'list-2', 'list-3', 'list-4', 'list-5'],
 };
 
 export default data;
